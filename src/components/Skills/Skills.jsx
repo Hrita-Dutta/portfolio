67f5aa4e45ce0c9805597e8cd42c9fccc5ce7acf
HritaDutta <hritadutta@gmail.com>
2023-08-30 00:21:31 +0545

    designed about section without mobile responsiveness

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -7,7 +7,37 @@ import AppDesign from "../../../public/assests/app-design.png"
 function Skills() {
   return (
     <section id='skills'>
-        About section
+        <span className="skillTitle">
+            What I do
+        </span>
+        <span className="skillDesc">I have embarked on a journey to understand the basics of web design, including HTML and CSS. While my experience is still growing, I am dedicated to honing these fundamental coding languages to create simple and functional web pages.</span>
+        <div className="skillBars">
+            <div className="skillBar">
+                <img src={UIDesign} alt="UIDesign" className="skillBarImg" />
+                <div className="skillBarText">
+                    <h2>UI/UX Design</h2>
+                    <p>This is a demo text, you can write your own content here.</p>
+                </div>
+            </div>
+        </div>
+        <div className="skillBars">
+            <div className="skillBar">
+                <img src={WebDesign} alt="WebDesign" className="skillBarImg" />
+                <div className="skillBarText">
+                    <h2>WebDesign</h2>
+                    <p>This demo text can be changed while making the production ready website.</p>
+                </div>
+            </div>
+        </div>
+        <div className="skillBars">
+            <div className="skillBar">
+                <img src={AppDesign} alt="AppDesign" className="skillBarImg" />
+                <div className="skillBarText">
+                    <h2>AppDesign</h2>
+                    <p>You can write text related to mobile app development.</p>
+                </div>
+            </div>
+        </div>
     </section>
   )
 }
