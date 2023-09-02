371af3e1cd3066a2457b389ed1b579f339f44fa8
HritaDutta <hritadutta@gmail.com>
2023-09-02 23:57:15 +0545

    contact us page initiated

---
+++ src/components/Contact/contact.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import "./contact.css"
+import 
+
+function contact() {
+  return (
+    <section className='contactPage'>
+        <div id="clients">
+            <h1 className="contactPageTitle">My Clients</h1>
+            <p className="clientDesc">
+                I have had the opportunity to work with a diverse group of
+            </p>
+            <div className="clientImgs">
+                <img src={} alt="" className="clientImg" />
+                <img src={} alt="" className="clientImg" />
+                <img src={} alt="" className="clientImg" />
+                <img src={} alt="" className="clientImg" />
+            </div>
+        </div>
+         <div id="contact">
+
+         </div>
+    </section>
+  )
+}
+
+export default contact
