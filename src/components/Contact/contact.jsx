82e94f1c8dece8bbc560537b906ae45142d09279
HritaDutta <hritadutta@gmail.com>
2023-09-04 00:13:21 +0545

    created contact me section

--- src/components/Contact/contact.jsx
+++ src/components/Contact/contact.jsx
@@ -4,6 +4,10 @@ import Walmart from "../../../public/assests/walmart.png"
 import Adobe from "../../../public/assests/adobe.png"
 import Microsoft from "../../../public/assests/microsoft.png"
 import Facebook from "../../../public/assests/facebook.png"
+import FacebookIcon from "../../../public/assests/facebook-icon.png"
+import TwitterIcon from "../../../public/assests/twitter.png"
+import YoutubeIcon from "../../../public/assests/youtube.png"
+import InstagramIcon from "../../../public/assests/instagram.png"
 
 function Contact() {
   return (
@@ -21,7 +25,20 @@ function Contact() {
             </div>
         </div>
          <div id="contact">
-
+            <h1 className="contactPageTitle">Contact Me</h1>
+            <span className="contactDesc">Please fill out the form below to discuss any work opportunities</span>
+            <form action="" className="contactForm">
+                <input type="text" className="name" placeholder='Your Name'/>
+                <input type="email" className="email" placeholder='Your Email'/>
+                <textarea className='msg' name="message" rows="5" placeholder='Your Message'></textarea>
+                <button type="submit" value='Send' className="submitBtn">Submit</button>
+                <div className="links">
+                    <img src={FacebookIcon} alt="Facebook" className="link" />
+                    <img src={TwitterIcon} alt="Twitter" className="link" />
+                    <img src={YoutubeIcon} alt="Youtube" className="link" />
+                    <img src={InstagramIcon} alt="Instagram" className="link" />
+                </div>
+            </form>
          </div>
     </section>
   )
