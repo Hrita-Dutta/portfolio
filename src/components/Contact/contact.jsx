3e3502e98dbd0bcfafc024d24fc81b47ff5aa841
HritaDutta <hritadutta@gmail.com>
2023-09-05 20:15:50 +0545

    corrected the site

--- src/components/Contact/contact.jsx
+++ src/components/Contact/contact.jsx
@@ -42,7 +42,7 @@ function Contact() {
             <h1 className="contactPageTitle">Contact Me</h1>
             <span className="contactDesc">Please fill out the form below to discuss any work opportunities</span>
             <form className="contactForm" ref={form} onSubmit={sendEmail}>
-                <input type="text" className="name" placehol der='Your Name' name="from_name"/>
+                <input type="text" className="name" placeholder='Your Name' name="from_name"/>
                 <input type="email" className="email" placeholder='Your Email' name="from_email"/>
                 <textarea className='msg' name="message" rows="5" placeholder='Your Message'></textarea>
                 <button type="submit" value='Send' className="submitBtn">Submit</button>
